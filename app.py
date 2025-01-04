af76761d0a2ce555969c4db290de0022903b432b
KIRANKALLA <kallakiran1974@gmail.com>
2025-01-04 12:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ import streamlit as st
 import numpy as np
 from ultralytics import YOLO
 from PIL import Image
-from skimage.io import imread
 
 model = YOLO('wd.pt')
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
-    #image = np.array(Image.open(uploaded_file))
-    image = imread(uploaded_file)
+    image = np.array(Image.open(uploaded_file))
     results = model(image)
     annotated_image = results[0].plot()
     st.image(annotated_image, caption="Processed Image with Detections")
